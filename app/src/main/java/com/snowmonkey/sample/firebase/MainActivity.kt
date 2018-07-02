8504b4b89c156c88a8fa2c93e916ac538da12509
deergorilla <keigo136@gmail.com>
2018-07-02 13:02:39 +0900

    init firebase auth

---
+++ app/src/main/java/com/snowmonkey/sample/firebase/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.snowmonkey.sample.firebase
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.support.annotation.NonNull
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    private val email by lazy { findViewById<EditText>(R.id.email_text) }
+    private val password by lazy { findViewById<EditText>(R.id.password_text) }
+    val signin = findViewById<TextView>(R.id.signin_text)
+    val signup = findViewById<TextView>(R.id.signup_text)
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initFirebaseAuth()
+        initClickListener()
+    }
+    override fun onStart() {
+        super.onStart()
+        mAuth.addAuthStateListener(mAuthListener)
+    }
+    override fun onStop() {
+        super.onStop()
+        mAuth.removeAuthStateListener(mAuthListener)
+    }
+    fun initFirebaseAuth() {
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user!=null) {
+                // User is signed in
+                Log.d("aaaa", "onAuthStateChanged:signed_in:" + user.uid)
+            } else {
+                // User is signed out
+                Log.d("aaaa", "onAuthStateChanged:signed_out")
+            }
+            // ...
+        }
+    }
+    fun initClickListener() {
+        signin.setOnClickListener {
+        }
+
+        signup.setOnClickListener {
+            mAuth.signInWithEmailAndPassword(email.text.toString(),password.text.toString())
+        }
+    }
+
+}
